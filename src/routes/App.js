600744e48594f9f8605a1dd22a8eefa6383a7bb2
Briam <briammartinez@gmail.com>
2019-09-10 23:57:10 +0200

    feat(js): Created Router file

---
+++ src/routes/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+const App = () => {
+  <Router>
+    <Switch>
+      {/* Route here */}
+    </Switch>
+  </Router>
+}
+
+export default App;
