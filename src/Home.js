55ec421558eba2d7045a31fafab8bc2df7df3650
Briam Martinez Escobar <briammartinez@gmail.com>
2019-09-11 13:53:39 +0200

    Feature/husky config (#3)
    * chore: Added eslint configuration
    
    * chore: .eslintignore was created
    
    * chore: Modify eslint configuration
    
    * Feature/router (#2)
    
    * chore(npm): Added react-router-dom dependency
    
    * feat(js): Created Router file
    
    * feat(*): Added simple route
    
    * feat: Added husky to the project

--- src/Home.js
+++ src/Home.js
@@ -2,11 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Home() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={ logo } className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -23,4 +23,4 @@ function App() {
   );
 }
 
-export default App;
+export default Home;
