e9cb4c642867a372d39524e43add253904d562d2
Briam <briammartinez@gmail.com>
2019-09-16 01:14:56 +0200

    feat: Added default input of the app

---
+++ src/components/commons/Input/Input.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './input.module.css';
+
+const Input = ({ className = '', error, ...res }) => (
+  <input className={`${ className } ${ styles.defaultInput } ${ error ? styles.error : '' }`} {...res} />
+);
+
+export default Input;
