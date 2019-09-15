dadd86dacba442d9994c7220fda3fd08fc5eec0a
Briam <briammartinez@gmail.com>
2019-09-15 21:55:19 +0200

    feat: Added Login route

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import Home from '../Home'
+import Home from '../Home';
+import Login from '../components/Login/Login.js';
 
 const App = () => (
   <Router>
     <Switch>
       <Route path="/" exact component={ Home }/>
+      <Route path="/login" exact component={ Login }/>
     </Switch>
   </Router>
 );
