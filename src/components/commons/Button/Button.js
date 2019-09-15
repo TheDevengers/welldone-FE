97592e6fd1fa03fda7986f71d246b3c7f2a8c650
Briam <briammartinez@gmail.com>
2019-09-16 01:13:33 +0200

    feat: Added default button of the app

---
+++ src/components/commons/Button/Button.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './button.module.css';
+
+const Button = ({ children, className, ...res }) => (
+  <button className={`${ styles.defaultButton } ${ className }`} {...res}>{children}</button>
+);
+
+export default Button;
