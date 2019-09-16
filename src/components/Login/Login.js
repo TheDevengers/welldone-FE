3d855f2604a0c5acc7e5dfb18872c778b444ae9d
Briam <briammartinez@gmail.com>
2019-09-17 00:05:47 +0200

    refactor: Refactor of Login component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,12 +2,18 @@ import React from 'react';
 import { Formik } from 'formik';
 import * as Yup from 'yup';
 
+import styles from './login.module.css';
 import logo from '../../assets/logo-beta.png';
 import { Input, Button } from '../commons';
-import styles from './login.module.css';
+
+import api from '../../utils/api';
+import cookieStorage from '../../utils/cookieStorage';
+
+const { postUser } = api();
+const { set } = cookieStorage();
 
 const SignupSchema = Yup.object().shape({
-  email: Yup.string().email('Must be an email').required('Email is required'),
+  username: Yup.string().required('Username is required'),
   password: Yup.string().required('Password is required')
 });
 
@@ -15,33 +21,33 @@ const Login = () => (
   <div className={styles.login_container}>
     <div className={styles.icon_container}>
       <picture>
-        <img data-cy="login-image" src={logo} alt="movie" className={styles.logo} />
+        <img src={logo} alt="movie" className={styles.logo} />
       </picture>
     </div>
     <Formik
       initialValues={{
-        email: '',
+        username: '',
         password: ''
       }}
       validationSchema={SignupSchema}
       onSubmit={(values) => {
-        console.log('Aqui post al backend', values);
+        postUser(values);
       }}
     >
       {({ handleSubmit, errors, touched, handleChange, values }) => (
-        <form onSubmit={handleSubmit} noValidate>
+        <form className={styles.form} onSubmit={handleSubmit} noValidate>
           <div className={styles.form_group}>
-            <label className={styles.form_label} htmlFor="email">Email:</label>
+            <label className={styles.form_label} htmlFor="text">Username:</label>
             <Input
-              type="email"
-              name="email"
-              placeholder="Email"
-              error={errors.email && touched.email}
+              type="text"
+              name="username"
+              placeholder="Enter a username..."
+              error={errors.username && touched.username}
               onChange={handleChange}
-              value={values.email}
+              value={values.username}
             />
-            {errors.email && touched.email ? (
-              <div className={styles.error}>{errors.email}</div>
+            {errors.username && touched.username ? (
+              <div className={styles.error}>{errors.username}</div>
             ) : null}
           </div>
           <div className={styles.form_group}>
@@ -49,7 +55,7 @@ const Login = () => (
             <Input
               type="password"
               name="password"
-              placeholder="Password"
+              placeholder="Enter a password..."
               error={errors.password && touched.password}
               onChange={handleChange}
               value={values.password}
@@ -58,8 +64,7 @@ const Login = () => (
               <div className={styles.error}>{errors.password}</div>
             ) : null}
           </div>
-          <Button className={styles.prueba}
-          type="submit">Submit</Button>
+          <Button type="submit">Submit</Button>
         </form>
       )}
     </Formik>
