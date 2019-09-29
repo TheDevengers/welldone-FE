819d098d64d63165462aa2dc3c2219711d434c54
Briam <briammartinez@gmail.com>
2019-09-30 01:36:37 +0200

    feat: Added edit-article route

--- src/routes/App.js
+++ src/routes/App.js
@@ -5,6 +5,7 @@ import PrivateRoute from '../components/PrivateRoute';
 import Login from '../components/Login/Login.js';
 import Signup from '../components/Signup/Signup.js';
 import ArticleForm from '../components/ArticleForm';
+import EditItemForm from '../components/EditItemForm/EditItemForm.js';
 import { Nav, Footer } from '../components/commons';
 import ListItems from '../components/ListItems/ListItems.js';
 
@@ -15,6 +16,7 @@ const App = () => (
       <Route path="/signup" exact component={Signup}/>
       <PrivateRoute path="/" redirectRoute="/login" exact component={ListItems}/>
       <PrivateRoute path="/create-article" exact component={ArticleForm} />
+      <PrivateRoute path="/edit-article" exact component={EditItemForm} />
     </Switch>
     <Footer />
   </Router>
