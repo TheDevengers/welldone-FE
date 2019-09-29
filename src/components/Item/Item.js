b60c5d6f33b15c9f0aa2ec0f8b09e6c7f68916bc
Briam <briammartinez@gmail.com>
2019-09-30 00:51:32 +0200

    feat: First version of Item component

---
+++ src/components/Item/Item.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import styles from './item.module.css';
+
+const Item = ({ data, ...props }) => {
+  return(
+    <div className={styles.item_container}>
+      <picture>
+        <img src={data.image} alt="article" className={styles.image_item} />
+      </picture>
+      <div className={styles.content_container}>
+        <div className={styles.nav_content}>
+          <h1 className={styles.content_title}>{data.title}</h1>
+          <div className={styles.icons}>
+            <i className="fas fa-trash delete" onClick={() => props.deleteArticle(data.id)}></i>
+            <i className="fas fa-edit edit"></i>
+          </div>
+        </div>
+        <p className={styles.item_description}>{data.introduction}</p>
+        <em>{data.publication_date}</em>
+      </div>
+    </div>
+  );
+};
+
+Item.defaultProps = {
+  deleteArticle: () => 0,
+  data: [],
+  image: '',
+  title: '',
+  introduction: '',
+  publication_date: ''
+
+};
+
+export default Item;
