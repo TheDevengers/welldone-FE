e3ca1b013f56be11f8a930fa1878e23d73770d9b
Briam <briammartinez@gmail.com>
2019-09-30 00:53:41 +0200

    feat: Added privateRoute with ListItems component

--- src/routes/App.js
+++ src/routes/App.js
@@ -2,18 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import PrivateRoute from '../components/PrivateRoute';
 
-import Home from '../Home';
 import Login from '../components/Login/Login.js';
 import Signup from '../components/Signup/Signup.js';
 import ArticleForm from '../components/ArticleForm';
 import { Nav, Footer } from '../components/commons';
+import ListItems from '../components/ListItems/ListItems.js';
 
 const App = () => (
   <Router>
     <Switch>
       <Route path="/login" exact component={Login}/>
-      <Route path="/signup" exact component={ Signup }/>
-      <PrivateRoute path="/" redirectRoute="/login" exact component={Home}/>
+      <Route path="/signup" exact component={Signup}/>
+      <PrivateRoute path="/" redirectRoute="/login" exact component={ListItems}/>
       <PrivateRoute path="/create-article" exact component={ArticleForm} />
     </Switch>
     <Footer />
