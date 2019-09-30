debfe1d7d61145865ed8f5fe2e75ee86ad563fa8
Briam <briammartinez@gmail.com>
2019-10-01 01:50:29 +0200

    chore(eslint): Added no-debugger rule

--- src/persistence/edit.js
+++ src/persistence/edit.js
@@ -3,11 +3,9 @@ import api from '../utils/api';
 const { editArticle } = api();
 
 export const editItem = (id, values) => {
-  debugger;
   return editArticle(id, values)
   .then(() => {
     window.location.href = '/';
   })
   .catch(() => new Error());
-
 };
