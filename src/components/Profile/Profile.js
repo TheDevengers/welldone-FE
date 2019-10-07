fba9aa4b5f44dab08f8f8ba228a096dd8c866cf9
Briam <briammartinez@gmail.com>
2019-10-08 00:02:46 +0200

    feat: Added desktop styles to profile component

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -3,7 +3,7 @@ import styles from './profile.module.css';
 import Button from '../commons/Button/Button';
 
 const Profile = () => (
-  <div className={styles.profile_container}>
+  <div className={styles.userprofile_container}>
     <div className={styles.up_container}>
       <div className={styles.img_container}>
         <img src="https://avatars2.githubusercontent.com/u/24435223?s=400&u=b024e15a5157673adf007ea2049f96c810dee4c9&v=4"
@@ -14,6 +14,11 @@ const Profile = () => (
       <div>
         <h3>bri06</h3>
         <h4>Briam Martínez Escobar</h4>
+      </div>
+      <div className={styles.user_description}>
+        <p>El Lorem Ipsum fue concebido como un texto de relleno,
+          formateado de una cierta manera para permitir la presentación de elementos gráficos en documentos,
+            sin necesidad de una copia formal</p>
       </div>
       <div>
         <Button className={styles.edit_profile_btn}>Edit</Button>
