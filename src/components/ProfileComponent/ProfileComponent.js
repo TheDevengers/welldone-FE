b632d6da8357015177b83da5b595605acb0ec633
Briam <briammartinez@gmail.com>
2019-10-10 23:05:40 +0200

    feat: Deleted mocks of ProfileComponent

--- src/components/ProfileComponent/ProfileComponent.js
+++ src/components/ProfileComponent/ProfileComponent.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import styles from './profileComponent.module.css';
 import Button from '../commons/Button/Button';
 
-const ProfileComponent = () => (
+const ProfileComponent = ({ profileInfo  }) => (
   <div className={styles.userprofile_container}>
     <div className={styles.up_container}>
       <div className={styles.img_container}>
@@ -13,13 +13,11 @@ const ProfileComponent = () => (
     </div>
     <div className={styles.low_container}>
       <div>
-        <h3>bri06</h3>
-        <h4>Briam Martínez Escobar</h4>
+        <h3>{profileInfo.username}</h3>
+        <h4>{`${profileInfo.first_name} ${profileInfo.last_name}`}</h4>
       </div>
       <div className={styles.user_description}>
-        <p>El Lorem Ipsum fue concebido como un texto de relleno,
-          formateado de una cierta manera para permitir la presentación de elementos gráficos en documentos,
-            sin necesidad de una copia formal</p>
+        <p>{profileInfo.profile.description}</p>
       </div>
       <div>
         <Link to='/edit-profile'>
