f3f36b528d99627902592cda631b9addd5ff074b
Briam <briammartinez@gmail.com>
2019-10-12 21:49:43 +0200

    fix: fixed conflitcs

--- src/components/ListItems/ListItems.js
+++ src/components/ListItems/ListItems.js
@@ -2,15 +2,20 @@ import React, { Component } from 'react';
 import styles from './listItems.module.css';
 import Item from '../Item/Item.js';
 import api from '../../utils/api';
+import { Spinner, ModalBox } from '../commons/index';
+
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const { getArticles, deleteArticle, getFavorites } = api();
 
 class ListItems extends Component {
 
   constructor(props) {
-    super();
+    super(props);
     this.state = {
       articles: [],
+      loading: true
     };
   }
 
@@ -18,11 +23,12 @@ class ListItems extends Component {
     const { tab } = this.props;
     if(tab === 'articles') {
       getArticles()
-        .then((response) => {
-          this.setState({
-            articles: response
-          });
-        });
+      .then((response) => this.setState({ articles: response }))
+      .catch((err) => toast.error('Unable to load articles', {
+        autoClose: false,
+        position: toast.POSITION.BOTTOM_CENTER
+      }))
+      .finally(() => this.setState({ loading: false }));
     } else if (tab === 'favorites')  {
       getFavorites()
         .then((response) => {
@@ -37,11 +43,24 @@ class ListItems extends Component {
     deleteArticle(id);
     const filteredArticles = this.state.articles ? this.state.articles.filter((article) => article.id !== id) : [];
     this.setState({ articles: filteredArticles });
+    toast.success('Article deleted', {
+      autoClose: 1000,
+      position: toast.POSITION.BOTTOM_CENTER
+    });
+
   }
 
+    onHandleCloseModal = () => {
+      this.setState({ error: false });
+    };
+
   render() {
-    const { articles } = this.state;
+    const { articles, loading } = this.state;
     const { tab } = this.props;
+
+    if(loading) {
+      return <Spinner />;
+    }
 
     return(
       <div className={styles.items_container}>
