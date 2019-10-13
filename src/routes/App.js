f159ca34e0f9ba115e63dd7f3ee4890dfac58156
Briam <briammartinez@gmail.com>
2019-10-13 15:38:03 +0200

    feat: Added routeWithError function to file with the routes

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import PrivateRoute from '../components/PrivateRoute';
+import { ErrorContext } from '../context/ErrorProvider';
 
 import Login from '../components/Login/Login.js';
 import Signup from '../components/Signup/Signup.js';
@@ -10,15 +11,23 @@ import EditProfile from '../components/UserProfileForm/index';
 import { Footer, NoMatch } from '../components/commons';
 import Home from '../Home';
 
+const routeWithError = (Component) => (props) => (
+  <ErrorContext.Consumer>
+    {({ showError }) => (
+      <Component handleError={showError} {...props} />
+    )}
+  </ErrorContext.Consumer>
+);
+
 const App = () => (
   <Router>
     <Switch>
       <Route path="/login" exact component={Login}/>
       <Route path="/signup" exact component={Signup}/>
       <PrivateRoute path="/" redirectRoute="/login" exact component={Home}/>
-      <PrivateRoute path="/create-article" exact component={ArticleForm} />
-      <PrivateRoute path="/edit-article/:id" exact component={EditItem} />
-      <PrivateRoute path="/edit-profile" exact component={EditProfile} />
+      <PrivateRoute path="/create-article" exact component={routeWithError(ArticleForm)} />
+      <PrivateRoute path="/edit-article/:id" exact component={routeWithError(EditItem)} />
+      <PrivateRoute path="/edit-profile" exact component={routeWithError(EditProfile)} />
       <PrivateRoute component={NoMatch} />
     </Switch>
     <Footer />
