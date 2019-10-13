0658732a80aaa9e04a74b1c4e557d20cf07431c0
Briam <briammartinez@gmail.com>
2019-10-13 14:30:50 +0200

    feat: Added handleError to profileComponent

--- src/components/ProfileComponent/Index.js
+++ src/components/ProfileComponent/Index.js
@@ -6,6 +6,9 @@ import apiUserProfile from '../../utils/apiUserProfile';
 
 import { Spinner } from '../commons/index';
 
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const { get } = cookieStorage();
 const { getUserInfo } = apiUserProfile();
 
@@ -24,7 +27,14 @@ class Profile extends Component {
   componentDidMount() {
     getUserInfo(get('id'))
     .then((userInfo) => this.setState({ profileInfo: userInfo }))
-    .catch((err) => console.log(err))
+    .catch((err) => {
+      this.props.handleError(err, () => {
+        toast.error('Unable to load profile info', {
+          autoClose: false,
+          position: toast.POSITION.BOTTOM_CENTER
+        });
+      });
+    })
     .finally(() => this.setState({ loading: false }));
   }
 
