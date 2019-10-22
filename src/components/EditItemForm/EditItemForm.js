1f67bc7bd6eb0591c84b29a11916a1f2835f07a6
Briam <briammartinez@gmail.com>
2019-10-22 22:27:24 +0200

    feat: Added image input

--- src/components/EditItemForm/EditItemForm.js
+++ src/components/EditItemForm/EditItemForm.js
@@ -5,7 +5,6 @@ import * as Yup from 'yup';
 import styles from './editItemForm.module.css';
 
 import { Input, Button } from '../commons';
-// import Nav from '../commons/Nav/Nav.js';
 
 import { editItem } from '../../persistence/edit';
 
@@ -19,6 +18,7 @@ const EditItemSchema = Yup.object().shape({
   introduction: Yup.string().required('Introduction is required').min(4, 'Title must be between 4 and 150 characters')
     .max(250, 'Title must be between 4 and 150 characters'),
   state: Yup.string().required('State is required'),
+  image: Yup.string().required('State is required'),
   body: Yup.string().required('Body is required').min(4, 'Title must be between 4 and 150 characters')
     .max(2000, 'Title must be between 4 and 150 characters'),
   categories: Yup.array(),
@@ -30,6 +30,7 @@ const EditItemForm = ({ dataArticle, dataCategories }) => (
     initialValues={{
       title: dataArticle.title,
       introduction: dataArticle.introduction,
+      image: dataArticle.image,
       state: dataArticle.state,
       body: dataArticle.body,
       categories: []
@@ -89,6 +90,22 @@ const EditItemForm = ({ dataArticle, dataCategories }) => (
           </select>
           {errors.state && touched.state ? (
             <div className={styles.error}>{errors.state}</div>
+          ) : null}
+        </div>
+        <div className={styles.form_group}>
+          <label className={styles.form_label} htmlFor="text">Image:</label>
+          <Input
+            className={styles.input}
+            type="text"
+            name="image"
+            placeholder="Enter url of image..."
+            error={errors.image && touched.image}
+            onChange={handleChange}
+            value={values.image}
+            data-cy="edit-image-item"
+          />
+          {errors.image && touched.image ? (
+            <div className={styles.error}>{errors.image}</div>
           ) : null}
         </div>
         <div className={styles.form_group}>
