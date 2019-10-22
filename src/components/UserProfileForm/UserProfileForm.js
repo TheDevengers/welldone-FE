3ac1f5521777dce667ac03a29dc568fda05a8341
Briam <briammartinez@gmail.com>
2019-10-22 22:52:30 +0200

    styles: Format UserProfileForm

--- src/components/UserProfileForm/UserProfileForm.js
+++ src/components/UserProfileForm/UserProfileForm.js
@@ -17,30 +17,30 @@ const handleDelete = () => deleteUserProfile(get('id'));
 const SIZE = '20px';
 
 const UserProfileSchema = Yup.object().shape({
-    username: Yup.string()
-        .min(4, 'Must be between 4 and 150 characters')
-        .max(150, 'Must be between 4 and 150 characters')
-        .required('Username is required'),
-    email: Yup.string()
-        .email('Must be an email')
-        .required('Email is required'),
+  username: Yup.string()
+    .min(4, 'Must be between 4 and 150 characters')
+    .max(150, 'Must be between 4 and 150 characters')
+    .required('Username is required'),
+  email: Yup.string()
+    .email('Must be an email')
+    .required('Email is required'),
 
-    first_name:Yup.string()
-        .min(2, 'Must be between 2 and 30 characters')
-        .max(30, 'Must be between 2 and 30 characters'),
+  first_name:Yup.string()
+    .min(2, 'Must be between 2 and 30 characters')
+    .max(30, 'Must be between 2 and 30 characters'),
 
-    last_name: Yup.string()
-        .min(4, 'Must be between 4 and 150 characters')
-        .max(150, 'Must be between 4 and 150 characters'),
+  last_name: Yup.string()
+    .min(4, 'Must be between 4 and 150 characters')
+    .max(150, 'Must be between 4 and 150 characters'),
 
-    profile: Yup.object().shape({
-      description: Yup.string()
-          .max(150, 'Maximum of 150 characters'),
-      birth_place: Yup.string()
-          .max(70, 'Maximum of 70 characters'),
-      image_user: Yup.string()
-          .max(300, 'Maximum of 300 characters'),
-    })
+  profile: Yup.object().shape({
+    description: Yup.string()
+      .max(150, 'Maximum of 150 characters'),
+    birth_place: Yup.string()
+      .max(70, 'Maximum of 70 characters'),
+    image_user: Yup.string()
+      .max(300, 'Maximum of 300 characters'),
+  })
 });
 
 const UserProfileForm = ({ dataUserProfile, profileInfo }) => {
@@ -48,148 +48,147 @@ const UserProfileForm = ({ dataUserProfile, profileInfo }) => {
   const [ error, setError ] = useState(false);
   const [ dataError, setDataError ] = useState({});
 
-return(
-  <Formik
-    enableReinitialize={true}
-          initialValues={ {
-            first_name: dataUserProfile.first_name,
-            last_name: dataUserProfile.last_name,
-            username: dataUserProfile.username,
-            email: dataUserProfile.email,
-            profile: {
-              description: profileInfo  ?  profileInfo.description : '',
-              birth_date:  profileInfo ? profileInfo.birth_date : '',
-              birth_place: profileInfo ? profileInfo.birth_place : '',
-              image_user: profileInfo ? profileInfo.image_user : ''
-            },
-          } }
+  return(
+    <Formik
+      enableReinitialize={true}
+      initialValues={{
+        first_name: dataUserProfile.first_name,
+        last_name: dataUserProfile.last_name,
+        username: dataUserProfile.username,
+        email: dataUserProfile.email,
+        profile: {
+          description: profileInfo  ?  profileInfo.description : '',
+          birth_date:  profileInfo ? profileInfo.birth_date : '',
+          birth_place: profileInfo ? profileInfo.birth_place : '',
+          image_user: profileInfo ? profileInfo.image_user : ''
+        },
+      }}
 
-          validationSchema={ UserProfileSchema }
+      validationSchema={ UserProfileSchema }
 
-          onSubmit = { async (values) => {
-            
-            if (!values.profile.description){
-              values.profile.description = null;
-            }
+      onSubmit = { async (values) => {
 
-            if (!values.profile.birth_place){
-              values.profile.birth_place = null;
-            }
+        if (!values.profile.description){
+          values.profile.description = null;
+        }
 
-            if (!values.profile.birth_date){
-              values.profile.birth_date = null;
-            }else if (!values.profile.birth_date.length) {
-              values.profile.birth_date = `${values.profile.birth_date.getFullYear()}-${values.profile.birth_date.getMonth()+1}-${values.profile.birth_date.getDate()}`;
-            }
+        if (!values.profile.birth_place){
+          values.profile.birth_place = null;
+        }
 
-            if (!values.profile.image_user){
-              values.profile.image_user = null;
-            }
- 
-            const response = await editUserProfile(get('id'), values);
-            if (response && response.error) {
-              setError(response.error);
-              setDataError(response);
-            } else {
-              window.location.href = '/';
-            }
+        if (!values.profile.birth_date){
+          values.profile.birth_date = null;
+        }else if (!values.profile.birth_date.length) {
+          values.profile.birth_date = `${values.profile.birth_date.getFullYear()}-${values.profile.birth_date.getMonth()+1}-${values.profile.birth_date.getDate()}`;
+        }
 
-          }}
-        >
+        if (!values.profile.image_user){
+          values.profile.image_user = null;
+        }
 
-    {({ errors, touched, values, setFieldValue, handleBlur }) => (
-      <Form noValidate className={styles.form}>
-        <h1 className={styles.title}>User details</h1>
-        <div className={styles.form_group}>
-          <label htmlFor="first_name">First Name:</label>
-          <Field data-cy="first-name-label" name="first_name" type="text" value={values.first_name}
-          className={ 'form-control' + (errors.first_name && touched.first_name ? ' is-invalid' : '') } />
-          <ErrorMessage name="first_name" component="div" className="invalid-feedback" />
-          {
+        const response = await editUserProfile(get('id'), values);
+        if (response && response.error) {
+          setError(response.error);
+          setDataError(response);
+        } else {
+          window.location.href = '/';
+        }
+
+      }}
+    >
+
+      {({ errors, touched, values, setFieldValue, handleBlur }) => (
+        <Form noValidate className={styles.form}>
+          <h1 className={styles.title}>User details</h1>
+          <div className={styles.form_group}>
+            <label htmlFor="first_name">First Name:</label>
+            <Field data-cy="first-name-label" name="first_name" type="text" value={values.first_name}
+            className={ 'form-control' + (errors.first_name && touched.first_name ? ' is-invalid' : '') } />
+            <ErrorMessage name="first_name" component="div" className="invalid-feedback" />
+            {
               error && dataError.first_name ? (
                 <div className={styles.error}>{dataError.first_name}</div>
               ) : null
-            }
-        </div>
+              }
+          </div>
 
-        <div className={styles.form_group}>
-          <label htmlFor="last_name">Last Name:</label>
-          <Field data-cy="last-name-label" name="last_name" type="text" value={values.last_name} onBlur={handleBlur}
-          className={ 'form-control' + (errors.last_name && touched.last_name ? ' is-invalid' : '') } />
-          <ErrorMessage name="last_name" component="div" className="invalid-feedback" />
-          {
+          <div className={styles.form_group}>
+            <label htmlFor="last_name">Last Name:</label>
+            <Field data-cy="last-name-label" name="last_name" type="text" value={values.last_name} onBlur={handleBlur}
+            className={ 'form-control' + (errors.last_name && touched.last_name ? ' is-invalid' : '') } />
+            <ErrorMessage name="last_name" component="div" className="invalid-feedback" />
+            {
               error && dataError.last_name ? (
                 <div className={styles.error}>{dataError.last_name}</div>
               ) : null
             }
-        </div>
+          </div>
 
-        <div className={styles.form_group}>
-          <label htmlFor="username">Username:</label>
-          <Field data-cy="username-label" name="username" type="text" value={values.username} onBlur={handleBlur}
-          className={ 'form-control' + (errors.username && touched.username ? ' is-invalid' : '') } />
-          <ErrorMessage name="username" component="div" className="invalid-feedback" />
-          {
+          <div className={styles.form_group}>
+            <label htmlFor="username">Username:</label>
+            <Field data-cy="username-label" name="username" type="text" value={values.username} onBlur={handleBlur}
+            className={ 'form-control' + (errors.username && touched.username ? ' is-invalid' : '') } />
+            <ErrorMessage name="username" component="div" className="invalid-feedback" />
+            {
               error && dataError.username ? (
                 <div className={styles.error}>{dataError.username}</div>
               ) : null
             }
-        </div>
-        <div className={styles.form_group}>
-          <label htmlFor="email">Email:</label>
-          <Field data-cy="email-label" name="email" type="text" value={values.email}
-          className={ 'form-control' + (errors.email && touched.email ? ' is-invalid' : '') } />
-          <ErrorMessage name="email" component="div" className="invalid-feedback" />
-          {
+          </div>
+          <div className={styles.form_group}>
+            <label htmlFor="email">Email:</label>
+            <Field data-cy="email-label" name="email" type="text" value={values.email}
+            className={ 'form-control' + (errors.email && touched.email ? ' is-invalid' : '') } />
+            <ErrorMessage name="email" component="div" className="invalid-feedback" />
+            {
               error && dataError.email ? (
                 <div className={styles.error}>{dataError.email}</div>
               ) : null
             }
-        </div>
+          </div>
 
-        <div className={styles.form_group}>
-          <label htmlFor="profile.image_user">Profile Picture:</label> 
-          <Field data-cy="image-user-label" name="profile.image_user" type="text" value={values.profile.image_user} onBlur={handleBlur}
-          className={ 'form-control' + (getIn(errors, 'profile.image_user') && getIn(touched, 'profile.image_user') ? ' is-invalid' : '') } />
-          <ErrorMessage name="profile.image_user" component="div"  className="invalid-feedback" />            
-        </div>
+          <div className={styles.form_group}>
+            <label htmlFor="profile.image_user">Profile Picture:</label>
+            <Field data-cy="image-user-label" name="profile.image_user" type="text" value={values.profile.image_user} onBlur={handleBlur}
+            className={ 'form-control' + (getIn(errors, 'profile.image_user') && getIn(touched, 'profile.image_user') ? ' is-invalid' : '') } />
+            <ErrorMessage name="profile.image_user" component="div"  className="invalid-feedback" />
+          </div>
 
-        <div className={styles.form_group}>
-          <label htmlFor="profile.description">Description:</label>
-          <Field data-cy="description-label" name="profile.description" type="text" value={values.profile.description} onBlur={handleBlur}
-          className={ 'form-control' + (getIn(errors, 'profile.description') && getIn(touched, 'profile.description') ? ' is-invalid' : '') } />
-          <ErrorMessage name="profile.description" component="div" className="invalid-feedback" />            
-        </div>
+          <div className={styles.form_group}>
+            <label htmlFor="profile.description">Description:</label>
+            <Field data-cy="description-label" name="profile.description" type="text" value={values.profile.description} onBlur={handleBlur}
+            className={ 'form-control' + (getIn(errors, 'profile.description') && getIn(touched, 'profile.description') ? ' is-invalid' : '') } />
+            <ErrorMessage name="profile.description" component="div" className="invalid-feedback" />
+          </div>
 
-        <div className={styles.form_group}>
-          <label htmlFor="profile.birth_place">Birth place:</label>
-          <Field data-cy="birth-place-label" name="profile.birth_place" type="text" value={values.profile.birth_place} onBlur={handleBlur}
-          className={ 'form-control' + (getIn(errors, 'profile.birth_place') && getIn(touched, 'profile.birth_place') ? ' is-invalid' : '') } />
-          <ErrorMessage name="profile.birth_place" component="div" className="invalid-feedback" />            
-        </div>
-   
-        <div className={styles.form_group}>
-          <label htmlFor="profile.birthdate">Birth Date:</label>           
-          <DatePicker name="profile.birth_date" value={ values.profile.birth_date } onChange={ setFieldValue } 
-          className={ 'form-control' + (getIn(errors, 'profile.birth_date') && getIn(touched, 'profile.birth_date') ? ' is-invalid' : '') } />
-          <ErrorMessage name="profile.birth_date" component="div" className="invalid-feedback" />
-        </div>
+          <div className={styles.form_group}>
+            <label htmlFor="profile.birth_place">Birth place:</label>
+            <Field data-cy="birth-place-label" name="profile.birth_place" type="text" value={values.profile.birth_place} onBlur={handleBlur}
+            className={ 'form-control' + (getIn(errors, 'profile.birth_place') && getIn(touched, 'profile.birth_place') ? ' is-invalid' : '') } />
+            <ErrorMessage name="profile.birth_place" component="div" className="invalid-feedback" />
+          </div>
 
-        <Button data-cy="profile-form-btn" type="submit" className={styles.submit}>Edit</Button>
-          
-        <ConfirmDialog title="Delete User" description="The user will be permanently deleted, are you sure you want to delete it?">
-          {(confirm) => (
-            <ButtonDelete className={styles.delete} onClick={confirm(handleDelete)}>
-              <MdDeleteForever size={SIZE}/>Delete account
-            </ButtonDelete>
-                
-          )}
-        </ConfirmDialog>
+          <div className={styles.form_group}>
+            <label htmlFor="profile.birthdate">Birth Date:</label>
+            <DatePicker name="profile.birth_date" value={ values.profile.birth_date } onChange={ setFieldValue }
+            className={ 'form-control' + (getIn(errors, 'profile.birth_date') && getIn(touched, 'profile.birth_date') ? ' is-invalid' : '') } />
+            <ErrorMessage name="profile.birth_date" component="div" className="invalid-feedback" />
+          </div>
 
-      </Form>
-          )}
-  </Formik>
-      
-      );};
+          <Button data-cy="profile-form-btn" type="submit" className={styles.submit}>Edit</Button>
+
+          <ConfirmDialog title="Delete User" description="The user will be permanently deleted, are you sure you want to delete it?">
+            {(confirm) => (
+              <ButtonDelete className={styles.delete} onClick={confirm(handleDelete)}>
+                <MdDeleteForever size={SIZE}/>Delete account
+              </ButtonDelete>
+            )}
+          </ConfirmDialog>
+
+        </Form>
+      )}
+    </Formik>
+  );
+};
 
 export default UserProfileForm;
--- src/persistence/access.js
+++ src/persistence/access.js
@@ -1,5 +1,4 @@
 import api from '../utils/api';
-import { reject } from 'q';
 import cookieStorage from './cookieStorage';
 
 const { postUser } = api();
